d5e575f0216247f016dd937e7fe7c5a930c19879
Andrew Siegle <siegleal@rose-hulman.edu>
2011-10-31 23:07:43 -0400

    removed the rose site

--- httrack-3.44.1/www.rose-hulman.edu/scripts/global.js
+++
@@ -1,8 +0,0 @@
-function setMouseover(selector, onPath, offPath) {
-  $(selector).mouseover(function() {
-    $(this).attr("src", onPath);
-  })
-  .mouseout(function(){
-    $(this).attr("src", offPath);
-  });
-}
