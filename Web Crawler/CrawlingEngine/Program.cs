7d23304fadc7eea1cd15168354143422652cd403
unknown <siegleal@.rose-hulman.edu>
2012-01-09 20:34:25 -0500

    basic prototype, does bacis crawling

---
+++ Web Crawler/CrawlingEngine/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the site to crawl>>");
+            String path = Console.ReadLine();
+            Console.Write("Enter the crawl level>>");
+            String level = Console.ReadLine();
+
+            CrawlingEngine.Crawler crawler = new CrawlingEngine.Crawler(path, Int32.Parse(level));            
+            
+            Console.Read();
+        }
+    }
