15553ecee54e929df771a131e23cfc0d2c90d7cd
unknown <siegleal@.rose-hulman.edu>
2012-01-19 10:11:05 -0500

    added logger to solution

---
+++ CrawlerSolution/Crawler/Log.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Logger
+{
+    class Log
+    {
+        System.IO.StreamWriter file;
+        string format;
+
+        public Log(string filePath)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter(filePath);
+        }
+
+        void writeWarning(string text)
+        {
+            file.WriteLine(DateTime.Now.ToString("yyyy.MM.dd hh:mm:ss")+"   WARNING: "+text);
+        }
+
+        void writeError(string text)
+        {
+            file.WriteLine(DateTime.Now.ToString("yyyy.MM.dd hh:mm:ss") + "   ERROR: " + text);
+        }
+
+        void writeFatal(string text)
+        {
+            file.WriteLine(DateTime.Now.ToString("yyyy.MM.dd hh:mm:ss") + "   FATAL: " + text);
+        }
+
+        void writeInfo(string text)
+        {
+            file.WriteLine(DateTime.Now.ToString("yyyy.MM.dd hh:mm:ss") + "   INFO: " + text);
+        }
+
+        void writeDebug(string text)
+        {
+            file.WriteLine(DateTime.Now.ToString("yyyy.MM.dd hh:mm:ss") + "   DEBUG: " + text);
+        }
+
+        void writeUser(string text, string type)
+        {
+            file.WriteLine(DateTime.Now.ToString("yyyy.MM.dd hh:mm:ss") + "   "+type+": " + text);
+        }
+    }
+}
