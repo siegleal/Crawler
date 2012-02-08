6ecd7d2c9358ca083b034ce66735078c16c94241
Andrew Siegle <siegleal@rose-hulman.edu>
2012-02-08 17:49:08 -0500

    controller stuff

--- CrawlerSolution/Crawler/CrawlerController.cs
+++ CrawlerSolution/Crawler/CrawlerController.cs
@@ -11,7 +11,7 @@ namespace Crawler
     {
         public CrawlerController(String path, int depth)
         {
-            //TODO logging, we don't have the logger :(
+           
             String outputPath = path.Substring(path.IndexOf('.') + 1,path.LastIndexOf('.') - path.IndexOf('.') -1) + "-" + DateTime.Now.Month.ToString().PadLeft(2, '0') + DateTime.Now.Day.ToString().PadLeft(2, '0') + DateTime.Now.Year.ToString() + DateTime.Now.ToFileTime().ToString();
          
             String arguments = path + " -g -r" + depth.ToString() + " -O " + outputPath;
@@ -21,12 +21,19 @@ namespace Crawler
 
             outputPath =  Directory.GetCurrentDirectory() + "\\"  +  outputPath;
             
+            //initialize the website
             Website site = new Website(path,outputPath);
+
+            //initialize the log
             Log log = new Log(outputPath + "\\log.txt");
 
+            //initalize the database accessor class
+            DatabaseAccessor dbAccess = new DatabaseAccessor(log);
+            int crawlID = dbAccess.newCrawl(path,"example@gmail.com");
 
-            WebsiteParser siteparser = new WebsiteParser(site, null, 0, log);
-            siteparser.analyzeSite(site, null, 0, log);
+            //Parse website
+            WebsiteParser siteparser = new WebsiteParser(site, dbAccess, crawlID, log);
+            siteparser.analyzeSite();
 
             log.destroy();
         }
