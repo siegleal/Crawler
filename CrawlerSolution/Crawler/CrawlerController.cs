3c75bd66258795339d6096371e88f83148c95419
Andrew Siegle <siegleal@rose-hulman.edu>
2012-02-08 17:45:22 -0500

    added crawler controller

---
+++ CrawlerSolution/Crawler/CrawlerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+
+namespace Crawler
+{
+    public class CrawlerController
+    {
+        public CrawlerController(String path, int depth)
+        {
+            //TODO logging, we don't have the logger :(
+            String outputPath = path.Substring(path.IndexOf('.') + 1,path.LastIndexOf('.') - path.IndexOf('.') -1) + "-" + DateTime.Now.Month.ToString().PadLeft(2, '0') + DateTime.Now.Day.ToString().PadLeft(2, '0') + DateTime.Now.Year.ToString() + DateTime.Now.ToFileTime().ToString();
+         
+            String arguments = path + " -g -r" + depth.ToString() + " -O " + outputPath;
+            //DEBUG - Console.WriteLine("Running httrack with arguments: " + arguments);
+            Process p = Process.Start(Directory.GetCurrentDirectory() + "/httrack/httrack.exe", arguments);
+            p.WaitForExit();
+
+            outputPath =  Directory.GetCurrentDirectory() + "\\"  +  outputPath;
+            
+            Website site = new Website(path,outputPath);
+            Log log = new Log(outputPath + "\\log.txt");
+
+
+            WebsiteParser siteparser = new WebsiteParser(site, null, 0, log);
+            siteparser.analyzeSite(site, null, 0, log);
+
+            log.destroy();
+        }
+    }
+}
