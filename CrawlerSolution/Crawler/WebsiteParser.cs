04fa1d31fc58b4d959066537f4c72b3e087ab744
Andrew Siegle <siegleal@rose-hulman.edu>
2012-02-08 17:17:19 -0500

    added list funtionality to website

--- CrawlerSolution/Crawler/WebsiteParser.cs
+++ CrawlerSolution/Crawler/WebsiteParser.cs
@@ -6,24 +6,25 @@ using System.IO;
 
 namespace Crawler
 {
+    //This class parses all of the files found and logs all of them
     class WebsiteParser : CrawlerPlugin, PluginInterface
     {
-
-
         public WebsiteParser(Website website, DatabaseAccessor db, int crawlID, Log l)
             : base(website, db, crawlID, l)
         {
             
         }
 
-        public List<String> analyzeSite(Website web, DatabaseAccessor db, int crawlID, Log log)
+        public List<String> analyzeSite()
         {
             log.writeInfo("Beginning website directory parsing");
-            var files = from file in Directory.EnumerateFiles(web.dirpath /*+ "\\" + web.url*/) select file;
+            var files = from file in Directory.EnumerateFiles(website.dirpath) select file;
 
             foreach (var file in files)
             {
-                log.writeDebug("Found file " + file.ToString().Substring(file.LastIndexOf("\\") + 1));
+                String filePath = file.ToString().Substring(file.LastIndexOf("\\") + 1);
+                log.writeDebug("Found file " + filePath);
+                website.addFile(filePath);
             }
             log.writeDebug(files.Count<string>().ToString() + " files found");
             return new List<String>();
