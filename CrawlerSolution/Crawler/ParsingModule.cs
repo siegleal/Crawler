9d0f4771d451a56c567daafe15e11a79346c3ea5
Trevor Krenz <krenztr@rose-hulman.edu>
2012-02-13 20:07:32 -0500

    ParsingModule.cs did not update for some reason. Now it is updated.

--- CrawlerSolution/Crawler/ParsingModule.cs
+++ CrawlerSolution/Crawler/ParsingModule.cs
@@ -4,25 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Collections;
 using System.IO;
-//Nothing too sophisicated yet. This class will retrun the frequency of the given strings in another given string or text or a file.
 namespace Crawler
 {
-    class ParsingModule: CrawlerPlugin
+    /// <summary>
+    /// Class: HTMLParseingModule
+    /// Description:
+    /// This class is a crawler plugin reads vulnerability definitions from the file "Vuln.txt" to search for in HTML files. Definitions are in the format [String]\t[Description], 
+    /// where [String] the string to look for in all source files, and [Description] is a brief description of what vulnerability [String] represents. To load definitions, call loadDefinitionsFromFile (loaded automatically with analyzeSite()).
+    /// </summary>
+    class HTMLParsingModule: CrawlerPlugin
     {
-        public ParsingModule(Website w, DatabaseAccessor db, int id, Log log)
-            : base(w, db, id, log)
-        {
-
-        }
-
+        //These will be defined when loadDefinitionsFromFile is called
         private List<String> searchStringList;
         private List<String> descriptionList;
-
-        public void passInStrings(List<String> stringList)
+        public HTMLParsingModule(Website w, DatabaseAccessor db, int id, Log log)
+            : base(w, db, id, log)
         {
-            searchStringList = stringList;
+            searchStringList = new List<String>();
+            descriptionList = new List<String>();
         }
 
+        //Loads definitions. Call before doing anything else with this object.
         public void loadDefinitionsFromFile()
         {
             //Load from Vuln.txt
@@ -32,50 +34,56 @@ namespace Crawler
             while ((line = file.ReadLine()) != null)
             {
                 int s = line.IndexOf("\t");
-                searchStringList[i] = line.Substring(0, s - 1);
-                descriptionList[i] = line.Substring(s + 1, line.Length);
+                searchStringList.Add(line.Substring(0, s));
+                descriptionList.Add(line.Substring(s+1, line.Length-s-1));
                 i++;
             }
         }
 
+        //Finds the vulnerabilites. Called by the factory.
         public override List<String> analyzeSite()
         {
+            loadDefinitionsFromFile();
             List<String> files = website.getFilesFound();
             foreach (String file in files)
             {
                 List<int> histogram = getFrequencyFromFile(file);
-                int j = 0;
-                foreach (int n in histogram)
+                for (int j = 0; j < histogram.Count; j++)
                 {
+                    int n = histogram[j];
                     if (n > 0)
                     {
-                        //We've found something.
-                        log.writeDebug("Vulnerability '" + descriptionList[j] + "' found in file " + file + ". Frequency: " + n);
+                        //A string has matched -- we've found something.
+                        log.writeUser("'" + descriptionList[j] + "' found in file " + file.Substring(file.LastIndexOf("\\")) + ". Frequency: " + n, "VULN");
+                        //Insert any other actions to do after finding a vulnerability here.
+
+
                     }
                     j++;
                 }
             }
-            //Not sure what exactly to return yet.
             return new List<String>();
         }
         
+        //Simply generates a histogram from the vulnerability string list.
         public List<int> getFrequencyFromString(string text)
         {
             int n = searchStringList.Count;
-            List<int> rList = new List<int>(); //I can't make an array list have a certain type of elements, I will look into this
+            List<int> rList = new List<int>();
             for (int i = 0; i < n; i++)
             {
-                rList[i] = 0;
+                rList.Add(0);
                 int j = 0;
-                while ((i = text.IndexOf(searchStringList[i], j)) != -1)
+                while ((j = text.IndexOf(searchStringList[i], j)) != -1)
                 {
                     j += searchStringList[i].Length;
-                    rList[i] = (int)rList[i]+1;
+                    rList[i] = rList[i]+1;
                 }
             }
             return rList;
         }
 
+        //File verson of getFrequencyFromString()
         public List<int> getFrequencyFromFile(string file)
         {
             StreamReader fileStream = new StreamReader(file);
