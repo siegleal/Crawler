217f7b4649cb3134f3f4cc01be63ff9273c9f78c
unknown <siegleal@SIEGLEAL-1.rose-hulman.edu>
2012-05-15 09:21:20 -0400

    added usage informationa and automatic mode

--- CrawlerSolution/Crawler/Program.cs
+++ CrawlerSolution/Crawler/Program.cs
@@ -10,18 +10,30 @@ using System.Reflection;
     {
         static void Main(string[] args)
         {
+            if (args.Length == 3)
+            {
+                var crawler = new Crawler.CrawlerController(args[0], Int32.Parse(args[1]), args[2]);
 
-            Console.WriteLine("Version {0}", Assembly.GetEntryAssembly().GetName().Version.ToString());
-            Console.Write("Enter the site to crawl>>");
-            String path = Console.ReadLine();
-            Console.Write("Enter the crawl level>>");
-            String level = Console.ReadLine();
-            Console.Write("Enter e-mail address to respond to>>");
-            String email = Console.ReadLine();
+            }else if (args.Length == 0)
+            {
+                Console.WriteLine("Version {0}", Assembly.GetEntryAssembly().GetName().Version.ToString());
+                Console.Write("Enter the site to crawl>>");
+                String path = Console.ReadLine();
+                Console.Write("Enter the crawl level>>");
+                String level = Console.ReadLine();
+                Console.Write("Enter e-mail address to respond to>>");
+                String email = Console.ReadLine();
 
-            Crawler.CrawlerController crawler = new Crawler.CrawlerController(path, Int32.Parse(level),email);
+                Crawler.CrawlerController crawler = new Crawler.CrawlerController(path, Int32.Parse(level), email);
 
-            Console.WriteLine("Complete. Press any key to continue...");
-            Console.Read();
+                Console.WriteLine("Complete. Press any key to continue...");
+                Console.Read();
+            }else
+            {
+                Console.Out.WriteLine("Correct useage:\n\nInteractive:\n\tCrawler.exe\nAutomatic:\n\tCrawler.exe <url> <crawl level> <response email address>");
+                Console.Read();
+            }
+
+
         }
     }
